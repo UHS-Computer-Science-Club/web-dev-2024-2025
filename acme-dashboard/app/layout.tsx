c5e03053eeed82276c33d3567f6c699b619bfbad
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-10-17 22:51:36 +0000

    Prepare meeting materials

---
+++ acme-dashboard/app/layout.tsx
@@ -0,0 +1,13 @@
+import { inter } from "./ui/fonts";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
