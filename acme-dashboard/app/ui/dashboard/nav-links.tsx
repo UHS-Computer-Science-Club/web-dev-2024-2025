a25397204e485849a6cf85fd759151a098e4c434
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-11-30 02:56:03 +0000

    Enhance NavLinks component with dynamic styling and update cSpell configuration

--- acme-dashboard/app/ui/dashboard/nav-links.tsx
+++ acme-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import clsx from "clsx";
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -18,6 +24,8 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
@@ -26,7 +34,12 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                "bg-sky-100 text-blue-600": pathname === link.href,
+              }
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
